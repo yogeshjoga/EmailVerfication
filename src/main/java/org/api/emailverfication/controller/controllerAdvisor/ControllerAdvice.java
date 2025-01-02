4018d5661fecea517b7e3e61d9d98f41f6380684
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2025-01-02 23:14:31 +0530

    controller Advice

--- src/main/java/org/api/emailverfication/controller/controllerAdvisor/ControllerAdvice.java
+++ src/main/java/org/api/emailverfication/controller/controllerAdvisor/ControllerAdvice.java
@@ -1,6 +1,9 @@
 package org.api.emailverfication.controller.controllerAdvisor;
 
+import org.api.emailverfication.exceptions.UserNotFoundException;
 import org.api.emailverfication.exceptions.WrongOTPException;
+import org.api.emailverfication.exceptions.WrongPasswordException;
+import org.api.emailverfication.exceptions.WrongUserNameException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,6 +13,21 @@ public class ControllerAdvice {
 
     @ExceptionHandler(WrongOTPException.class)
     public ResponseEntity<Exception> wrongOTP(Exception ex){
+        return ResponseEntity.status(400).body(ex);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Exception> userNotFound(Exception ex){
+        return ResponseEntity.status(400).body(ex);
+    }
+
+    @ExceptionHandler(WrongPasswordException.class)
+    public ResponseEntity<Exception> wrongPassword(Exception ex){
+        return ResponseEntity.status(400).body(ex);
+    }
+
+    @ExceptionHandler(WrongUserNameException.class)
+    public ResponseEntity<Exception> wrongUserName(Exception ex){
         return ResponseEntity.status(400).body(ex);
     }
 
